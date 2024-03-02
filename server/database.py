56c96afc16326ac4148340a70057aafc61e51f4e
lyssC1 <acaputo1@students.kennesaw.edu>
2024-03-02 17:05:36 -0500

    searches by alcohol base

--- server/database.py
+++ server/database.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 from google.cloud import firestore
 
 firestore_client = firestore.Client.from_service_account_json('../server/env.json')
@@ -11,6 +11,23 @@ def get_mixeddrink_data():
     data = []
     for doc in documents:
         data.append(doc.to_dict())
+    return jsonify(data)
+
+@app.route('/api/mixeddrinkbyliquor', methods=['GET'])
+def get_mixeddrink_data_by_base():
+    filter = request.args.get('filter')
+
+    if filter:
+        query_ref = []
+        for i in range(1,16):
+            query_ref.append(collection_ref.where(f'Ingredient{i}', '==', filter).stream())
+    data = []
+    for query in query_ref:
+        query_result = [{**doc.to_dict(), "id": doc.id} for doc in query]
+        data.append(query_result)
+
+    data = [item for sublist in data for item in sublist]   
+    print(data)
     return jsonify(data)
 
 # Run the Flask application
