dd45032fccc8f059d223537a350e5ee0da02897e
lyssC1 <acaputo1@students.kennesaw.edu>
2024-03-15 18:27:55 -0400

    Survery drop downs

--- client/controllers/getData.js
+++ client/controllers/getData.js
@@ -1,13 +1,37 @@
 
 export const getAllDrinkDataByLiquorType = async (liquor_type) => {
-    const url = `http://localhost:8180/api/mixeddrinkwith?filter=${liquor_type.substring(0,1).toUpperCase()+liquor_type.substring(1).toLowerCase()}&dontwant=` 
+    const url = `http://localhost:8180/api/mixeddrinkwith?filter=${liquor_type.substring(0, 1).toUpperCase() + liquor_type.substring(1).toLowerCase()}&dontwant=`
     console.log("Awaiting response...");
     const response = await fetch(url)
-    const trial = await response.json();
-    
-    return await trial
+    return await response.json();
 }
 
 
 
+export const filterDrinksByIngredientsOR = async (filters, dontwants) => {
 
+    const filterList = ""
+    const dontWantList = ""
+
+    for (let i = 0; i < filters.length; i++) {
+        if (i === filters.length - 1) {
+            filterList += filters[i]
+        }
+        else {
+            filterList += filters[i] + '-'
+        }
+    }
+
+    for (let i = 0; i < dontwants.length; i++) {
+        if (i === filters.length - 1) {
+            dontWantList += filters[i]
+        }
+        else {
+            dontWantList += filters[i] + '-'
+        }
+    }
+    const url = `http://localhost:8180/api/mixeddrinkwith?filter=${filterList}&dontwant=${dontWantList}`
+    console.log("Awaiting response...");
+    const response = await fetch(url)
+    return await response.json();
+}
