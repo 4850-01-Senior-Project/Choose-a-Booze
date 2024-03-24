b6406fa2b74f3d62af0ee17744e4159e43aff780
lyssC1 <acaputo1@students.kennesaw.edu>
2024-03-24 16:39:05 -0400

    Adjusted refactor.

--- client/controllers/getData.js
+++ client/controllers/getData.js
@@ -1,5 +1,5 @@
 
-export const getAllDrinkData= async () => {
+export const getAllDrinkData = async () => {
     const url = `http://127.0.0.1:8081/api/alldrinks`
     console.log("Awaiting response...");
     const response = await fetch(url)
@@ -9,10 +9,10 @@ export const getAllDrinkData= async () => {
 // --------------------------------------------------
 
 export const filterDrinksByIngredientsOR = async (filters, dontwants) => {
-console.log(filters)
-  const filterList = format(filters)
-  const dontWantList = format(dontwants)
-  
+    console.log(filters)
+    const filterList = format(filters)
+    const dontWantList = format(dontwants)
+
     const url = `http://127.0.0.1:8081/api/mixeddrinkwith?filter=${filterList}&dontwant=${dontWantList}`
     console.log("Awaiting response...");
     const response = await fetch(url)
@@ -21,40 +21,40 @@ console.log(filters)
 
 // --------------------------------------------------
 
-const format = (array) => {
-  result = ""
-  for (let i = 0; i < array.length; i++) {
-    if (i === array.length - 1) {
-      result += array[i]
+const format = (filter) => {
+    result = ""
+    for (let i = 0; i < array.length; i++) {
+        if (i === dontwants.length - 1) {
+            result += `${filter[i] === "undefined" ? filter[i] : ""}`
+        }
+        else {
+            result += `${filter[i] === "undefined" ? filter[i] : ""},`
+        }
+        return result
     }
-    else {
-      result += filters[i] + '-'
+}
+
+    // --------------------------------------------------
+
+    export const getTags = async () => {
+        const url = `http://127.0.0.1:8081/api/alltags`
+        console.log("Awaiting response...");
+        const response = await fetch(url)
+        const list = await response.json()
+        const formattedTagList = list.map((item) => { return { name: item.tag.Name, tagid: item.id } })
+
+        return formattedTagList;
     }
-  }
-  return result
-}
 
-// --------------------------------------------------
+    // --------------------------------------------------
 
-export const getTags = async () => {
-    const url = `http://127.0.0.1:8081/api/alltags`
-    console.log("Awaiting response...");
-    const response = await fetch(url)
-    const list = await response.json()
-    const formattedTagList = list.map((item) => { return { name: item.tag.Name, tagid: item.id } })
-   
-    return formattedTagList;
-}
+    export const getTagsWithDrinks = async () => {
+        const url = `http://127.0.0.1:8081/api/tagswithdrinks`
+        console.log("Awaiting response...");
+        const response = await fetch(url)
+        const list = await response.json()
+        let count = 0;
+        const formattedTagIDList = list.map((item) => { return { tagid: item.Tag, drinkids: item.Drinks } })
 
-// --------------------------------------------------
-
-export const getTagsWithDrinks = async () => {
-  const url = `http://127.0.0.1:8081/api/tagswithdrinks`
-  console.log("Awaiting response...");
-  const response = await fetch(url)
-  const list = await response.json()
-  let count = 0;
-  const formattedTagIDList = list.map((item) => { return { tagid: item.Tag, drinkids: item.Drinks } })
-
-  return formattedTagIDList;
-}
+        return formattedTagIDList;
+    }
